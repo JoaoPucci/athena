594fc74f0175405cb936638805252d171fd1e27b
Joao Pucci <joao.pucci@live.com>
2020-10-07 10:58:02 -0300

    Use junit 5 instead of 4

--- src/test/java/tech/dtech/athena/AthenaApplicationTests.java
+++ src/test/java/tech/dtech/athena/AthenaApplicationTests.java
@@ -2,8 +2,10 @@ package tech.dtech.athena;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class AthenaApplicationTests {
 
 	@Test
--- src/test/java/tech/dtech/athena/login/controller/LoginControllerTest.java
+++ src/test/java/tech/dtech/athena/login/controller/LoginControllerTest.java
@@ -3,20 +3,17 @@ package tech.dtech.athena.login.controller;
 import java.net.URI;
 
 import org.hamcrest.core.StringContains;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles(value = "test")
