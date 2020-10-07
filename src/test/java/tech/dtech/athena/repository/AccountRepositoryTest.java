b4784e89c289d9478f1eea84d9efabe0807bd86d
Joao Pucci <joao.pucci@live.com>
2020-10-07 14:42:27 -0300

    Remove JUnit4

--- src/test/java/tech/dtech/athena/repository/AccountRepositoryTest.java
+++ src/test/java/tech/dtech/athena/repository/AccountRepositoryTest.java
@@ -1,7 +1,7 @@
 package tech.dtech.athena.repository;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
