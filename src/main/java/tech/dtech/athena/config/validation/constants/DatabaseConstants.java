187fb23b61d352e4eb3bdee507971a4c88a38fbc
Joao Pucci <joao.pucci@live.com>
2020-10-12 18:44:18 -0300

    Use a final class instead of an interface for constants

--- src/main/java/tech/dtech/athena/config/validation/constants/DatabaseConstants.java
+++ src/main/java/tech/dtech/athena/config/validation/constants/DatabaseConstants.java
@@ -1,6 +1,6 @@
 package tech.dtech.athena.config.validation.constants;
 
-public interface DatabaseConstants {
+public final class DatabaseConstants {
 
     public static final int DATABASE_STRING_MAX_LENGTH = 255;
 
