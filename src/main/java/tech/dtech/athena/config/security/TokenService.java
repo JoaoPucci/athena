10661a554d2f051d9d7158838cf9e8c9833a0607
Joao Pucci <joao.pucci@live.com>
2020-10-26 19:50:15 -0300

    Create interface for token service

--- src/main/java/tech/dtech/athena/config/security/TokenService.java
+++ src/main/java/tech/dtech/athena/config/security/TokenService.java
@@ -1,48 +1,13 @@
 package tech.dtech.athena.config.security;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import tech.dtech.athena.model.Account;
+public interface TokenService {
 
-@Service
-public class TokenService {
+    public String generate(Authentication authentication);
 
-    @Value("${athena.jwt.expiration_millis}")
-    private String expirationMillis;
+    public boolean isValid(String token);
 
-    @Value("${athena.jwt.secret}")
-    private String secret;
-
-    public String generate(Authentication authentication) {
-        Account loggedUser = (Account) authentication.getPrincipal();
-
-        Date now = new Date();
-
-        return Jwts.builder().setIssuer("DTech Athena API").setSubject(loggedUser.getId().toString()).setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Long.parseLong(expirationMillis)))
-                .signWith(SignatureAlgorithm.HS256, secret).compact();
-    }
-
-    public boolean isValid(String token) {
-        try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
-        } catch (Exception e) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public Long getAccountId(String token) {
-        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-        return Long.parseLong(claims.getSubject());
-    }
+    public Long getAccountId(String token);
 
 }
---
+++ src/main/java/tech/dtech/athena/config/security/TokenServiceImpl.java
@@ -0,0 +1,51 @@
+package tech.dtech.athena.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import tech.dtech.athena.model.Account;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+    @Value("${athena.jwt.expiration_millis}")
+    private String expirationMillis;
+
+    @Value("${athena.jwt.secret}")
+    private String secret;
+
+    @Override
+    public String generate(Authentication authentication) {
+        Account loggedUser = (Account) authentication.getPrincipal();
+
+        Date now = new Date();
+
+        return Jwts.builder().setIssuer("DTech Athena API").setSubject(loggedUser.getId().toString()).setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + Long.parseLong(expirationMillis)))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    @Override
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Long getAccountId(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+
+}
