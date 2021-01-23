3f3d14de91d3f1d4a6338b60f11d0f65214dcdd9
Joao Pucci <joao.pucci@live.com>
2021-01-23 17:48:19 -0300

    Remove wildcard import

--- src/main/java/tech/dtech/athena/document/controller/DocumentController.java
+++ src/main/java/tech/dtech/athena/document/controller/DocumentController.java
@@ -2,7 +2,11 @@ package tech.dtech.athena.document.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import tech.dtech.athena.document.model.DocumentType;
 import tech.dtech.athena.document.model.DocumentTypeDTO;
